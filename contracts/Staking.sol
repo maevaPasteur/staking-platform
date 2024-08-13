72050ba75590358362effc95b9dc13c6c7a5c8fb
maevaPasteur <maeva.pasteur.pro@gmail.com>
2024-08-13 22:34:17 +0800

    feat(staking): init owner and erc20 token

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -1,13 +1,27 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.18;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./Token.sol";
 
 contract Staking {
     address public owner;
+    IERC20 public stakingToken;
 
-    constructor() {
+    mapping(address => uint256) public balances;
+
+    constructor(Token _stakingToken) {
+        stakingToken = _stakingToken;
         owner = msg.sender;
+    }
+
+    function stake(uint256 _amount) external {
+        require(_amount > 0, "Amount must be greater than 0");
+        stakingToken.transferFrom(msg.sender, address(this), _amount);
+        balances[msg.sender] += _amount;
+    }
+
+    function getUserBalance(address _userAddress) external view returns (uint256) {
+        return balances[_userAddress];
     }
 }
