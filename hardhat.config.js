9c1170cdd846326b437b153f4fd3845198e72660
maevaPasteur <maeva.pasteur.pro@gmail.com>
2024-08-14 16:37:55 +0800

    feat(deploy): add github actions

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('solidity-coverage');
 
 module.exports = {
   solidity: "0.8.18",
   networks: {
     hardhat: {
       chainId: 1337,
+      rinkeby: {
+        url: process.env.ALCHEMY_API_KEY_URL,
+        accounts: [process.env.PRIVATE_KEY]
+      }
     },
   },
   paths: {
